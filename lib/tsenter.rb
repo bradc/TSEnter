90c32ebd0fd8ed16e23dba226de93c49a91e6d75
Brad Coish <brad.coish@gmail.com>
2012-02-24 00:01:31 -0400

    Split out executable portion of file to make RSpec testing easier.

--- lib/tsenter.rb
+++ lib/tsenter.rb
@@ -84,6 +84,8 @@ module TS
   class Entry    
     # raises TS::InvalidEntry exception if verification fails.
     def initialize entry
+      @raw_entry = entry
+      parse_entry @raw_entry
     end
 
   end
@@ -131,22 +133,4 @@ module TS
     end
   end
 
-end
-
-if __FILE__ == $0
-  # CommandLine will process program switches, if/when needed
-  # for now it simply strips off any switches and concatenates 
-  # the remaining.  Assuming the remaining is a ts entry.
-  args = CommandLine.new(ARGV)
-  # Assume some default entries, such as default local data store
-  # we'll assume for now that KirbyBase will be used as the local
-  # store.
-  config = TS::Configure.new do |conf|
-    # Only adapter for now, later will be updated to handle other
-    # commandline configurations. 
-    # E.g. of setting a text editor for long descriptions.
-    #  conf.editor='notepad.exe'
-    conf.db= DataStore.new(:adapter=>:KirbyBase, :local=>true)
-  end
-  TS::Enter.new(config).start(args)
 end
